c0e555e1fac984d4c8925edcbeeb150a02e6ac9a
Aditi Kannan <akannan2@wellesley.edu>
2020-07-13 11:04:55 -0400

    answer attempt1 to assignment07

--- src/Assignment07.jl
+++ src/Assignment07.jl
@@ -26,4 +26,85 @@ function normalizeDNA(seq)
     return seq # change to `return LongDNASeq(seq)` if you want to try to use BioSequences types
 end
 
+function composition(seq)
+    sequence = normalizeDNA(seq) # make uppercase string, check invalid bases
+    a = c = g = t = n = 0 # sets all 4 variables to `0`
+    for base in sequence
+        a = count(==('A'), sequence)
+        c = count(==('C'), sequence)
+        g = count(==('G'), sequence)
+        t = count(==('T'), sequence)
+        n = count(==('N'), sequence)
+    end
+    composition = Dict('A' => a, 'G' => g, 'T' => t, 'N' => n, 'C' => c)
+    return composition
+end
+
+function gc_content(seq)
+    comp = composition(seq)
+    gs = comp['G']
+    cs = comp['C']
+    seqlength = length(seq)
+    return (gs+cs) / (seqlength)
+end
+
+function complement(base::Char)
+    complements = Dict('A' => 'T',
+                       'T' => 'A',
+                       'G' => 'C',
+                       'C' => 'G',
+                       'N' => 'N')
+    
+    !(base in keys(complements)) && error("Invalid base $base")
+    return complements[base]
+end
+
+function complement(seq::AbstractString)
+    seq = normalizeDNA(seq)
+    seq = collect(seq)
+    complement_seq = []
+    for base in eachindex(seq)
+        new_seq = complement(seq[base])
+        push!(complement_seq, new_seq)
+    end
+    complement_seq = join(complement_seq)
+    return complement_seq
+end
+
+function reverse_complement(seq)
+    reverse_sequence = reverse(seq)
+    sequence = collect(reverse_sequence)
+    reverse_complement = map(complement, sequence)
+    reverse_complement = join(reverse_complement)
+end
+
+function parse_fasta(path)
+    header = []
+    sequence = []
+    seqonly = []
+    for line in eachline(path)
+        if startswith(line, '>')
+            if length(header) > length(sequence)
+                seqonlynew = join(seqonly)
+                push!(sequence, seqonlynew)
+                seqonly = []
+            end
+            headerline = split(line, '>')
+            headerline = headerline[2]
+            push!(header, headerline)
+        elseif line == ""
+            continue
+        else
+            #if line is not a header or an empty string, push it to seqonly
+            #keep pushing these lines until you reach another header or empty string line
+            #then join all of these lines together and push the sequence to sequence
+            line = normalizeDNA(line)
+            push!(seqonly, line) 
+        end
+    end
+    seqonlynew = join(seqonly)
+    push!(sequence, seqonlynew)
+    return(header, sequence)
+end
+
 end # module Assignment07
